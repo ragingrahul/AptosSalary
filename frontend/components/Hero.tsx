e0607f4237700acd08f1f76ae7f32b48351af12b
ragingrahul <raj17sarma@gmail.com>
2024-09-30 00:54:59 +0530

    Added Footer

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -4,39 +4,46 @@ import { GridBackgroundDemo } from './ui/Grid'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 import { ShimmerButton } from './ui/ShimmerButton'
 import { FaCircleArrowRight } from 'react-icons/fa6'
+import { PiHandDeposit } from 'react-icons/pi'
+import { GiReceiveMoney } from 'react-icons/gi'
 
 const Hero = () => {
-  return (
-    <div className='pb-20 pt-36'>
-        <div>
-            <Spotlight  className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
-            <Spotlight  className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
-            <Spotlight  className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
-        </div>
-        <GridBackgroundDemo />
-        <div className='flex justify-center relative my-20 z-10'>
-            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-100'>
-                    Streamlining payroll with precision and ease.
-                </h2>
-                <TextGenerateEffect 
-                    className='text-center text-[40px] md:text-5xl lg:text-6xl'
-                    words='Effortless Payroll for Happier, Thriving Teams!'
-                />
-                <p className='text-center md:tracking-wider mb-4 md:text-lg lg:text-xl '>
-                    Transforming payroll processes for clarity and employee satisfaction!
-                </p>
-                <a href='#about'>
-                    <ShimmerButton 
-                        title='Connect'
-                        icon={<FaCircleArrowRight  />}
-                        position='left'
+    return (
+        <div className='pb-20 pt-36'>
+            <div>
+                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
+                <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
+                <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
+            </div>
+            <GridBackgroundDemo />
+            <div className='flex justify-center relative my-20 z-10'>
+                <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                    <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-100'>
+                        Streamlining payroll with precision and ease.
+                    </h2>
+                    <TextGenerateEffect
+                        className='text-center text-[40px] md:text-5xl lg:text-6xl'
+                        words='Effortless Payroll for Happier, Thriving Teams!'
                     />
-                </a>
+                    <p className='text-center md:tracking-wider mb-4 md:text-lg lg:text-xl '>
+                        Transforming payroll processes for clarity and employee satisfaction!
+                    </p>
+                    <div className='flex flex-row gap-6' >
+                        <ShimmerButton
+                            title='Employee Login'
+                            icon={<GiReceiveMoney />}
+                            position='left'
+                        />
+                        <ShimmerButton
+                            title='Employeer Login'
+                            icon={<PiHandDeposit  />}
+                            position='left'
+                        />
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Hero
--- frontend/components/ui/ShimmerButton.tsx
+++ frontend/components/ui/ShimmerButton.tsx
@@ -12,7 +12,7 @@ export const ShimmerButton = ({
 }: Props) => {
   return (
     <div>
-        <button className={`inline-flex h-10 w-full animate-shimmer items-center justify-around rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 gap-3 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-1 focus:ring-slate-400  ${otherClasses}`}>
+        <button className={`inline-flex h-10 w-full animate-shimmer items-center justify-around rounded-full border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 gap-3 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-1 focus:ring-slate-400  ${otherClasses}`}>
           {position === 'left' && icon}
           {title}
           {position === 'right' && icon}
