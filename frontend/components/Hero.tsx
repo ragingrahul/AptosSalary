63b196e07a13fe15dfc659198d800398bccd5b70
ragingrahul <raj17sarma@gmail.com>
2024-09-29 14:52:39 +0530

    Added Grid

--- frontend/components/Hero.tsx
+++ frontend/components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { GridBackgroundDemo } from './ui/Grid'
 
 const Hero = () => {
   return (
@@ -8,6 +9,15 @@ const Hero = () => {
             <Spotlight  className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white'/>
             <Spotlight  className='top-10 left-full h-[80vh] w-[50vw]' fill='purple'/>
             <Spotlight  className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue'/>
+        </div>
+        <GridBackgroundDemo />
+        <div className='flex justify-center relative my-20 z-10'>
+            <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+                <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-100'>
+                    Streamlining payroll with precision and ease.
+                </h2>
+            </div>
+
         </div>
     </div>
   )
