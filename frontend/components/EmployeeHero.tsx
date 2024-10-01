bee9e54b0f56b0dc4ecc9b1c3cb6b882a34cbf6f
ragingrahul <raj17sarma@gmail.com>
2024-10-01 18:24:33 +0530

    Employee Page Timeline

--- frontend/components/EmployeeHero.tsx
+++ frontend/components/EmployeeHero.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Spotlight } from './ui/Spotlight'
 import { GridBackgroundDemo } from './ui/Grid'
 import EmployeeCardDetails from './EmployeeCardDetails'
+import Timeline from './ui/Timeline'
+import { paymentHistory } from '@/data'
 
 const EmployeeHero = () => {
     return (
-        <div className='pb-20 pt-36 w-full'>
+        <div className='pb-20 pt-10 w-full'>
             <div>
                 <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
                 <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
@@ -13,6 +15,7 @@ const EmployeeHero = () => {
             </div>
             <GridBackgroundDemo />
             <EmployeeCardDetails />
+            <Timeline items={paymentHistory}/>
         </div>
     )
 }
