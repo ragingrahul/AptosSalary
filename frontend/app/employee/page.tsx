b6f30c66393ec86a1b2416e359c2c9dd3da2afd3
ragingrahul <raj17sarma@gmail.com>
2024-10-01 13:12:19 +0530

    Added EmployeeCard

---
+++ frontend/app/employee/page.tsx
@@ -0,0 +1,12 @@
+import EmployeeHero from '@/components/EmployeeHero'
+import React from 'react'
+
+const Employee = () => {
+  return (
+    <div className='w-full'>
+        <EmployeeHero />
+    </div>
+  )
+}
+
+export default Employee
