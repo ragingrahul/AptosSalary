51b9bbf98ad650858719528b32bb8308ab34b6b8
ragingrahul <raj17sarma@gmail.com>
2024-10-04 14:17:36 +0530

    Added Employee Table

--- frontend/components/EmployerHero.tsx
+++ frontend/components/EmployerHero.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import { GridBackgroundDemo } from './ui/Grid'
 import { EmployerBento } from './ui/EmployerBento'
+import EmployerGraphs from './ui/EmployerGraphs'
+import { Spotlight } from './ui/Spotlight'
 
 const EmployerHero = () => {
-  return (
-    <div className='pb-20 pt-10 w-full h-100vh'>
-        <GridBackgroundDemo />
-        <EmployerBento />
-    </div>
-  )
+    return (
+        <div className='pb-20 pt-10 w-full h-100vh'>
+            <div>
+                <Spotlight className='-top-40 -left-10 md:-left-32 md:-top-20 h-screen' fill='white' />
+                <Spotlight className='top-10 left-full h-[80vh] w-[50vw]' fill='purple' />
+                <Spotlight className='top-28 left-80 h-[80vh] w-[50vw]' fill='blue' />
+            </div>
+            <GridBackgroundDemo />
+            <EmployerBento />
+            <EmployerGraphs />
+        </div>
+    )
 }
 
 export default EmployerHero
