d83cbfae87c4c32f78e3ba4fc94d0807e4c9358d
ragingrahul <raj17sarma@gmail.com>
2024-10-15 23:32:50 +0530

    Fixing Linting Issues

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports ={
+  "extends": ["next/core-web-vitals", "next/typescript"],
+  "rules": {
+      "@typescript-eslint/no-unused-vars": 0 // eslint-disable-line @typescript-eslint/no-unused-vars
+    }
+  
+}
